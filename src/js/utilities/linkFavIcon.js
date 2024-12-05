c9d48cf32a484e795a7746d8db8fdaacbc3f0399
julieoyen <juloye51070@stud.noroff.no>
2024-12-05 16:25:49 +0100

    bug: netlify error

--- src/js/utilities/linkFavIcon.js
+++ src/js/utilities/linkFavIcon.js
@@ -1,7 +1,9 @@
+import favicon from '/images/favicon.png';
+
 export function addFavicon() {
   const link = document.createElement('link');
   link.rel = 'icon';
   link.type = 'image/png';
-  link.href = '/images/favicon.png';
+  link.href = favicon;
   document.head.appendChild(link);
 }
