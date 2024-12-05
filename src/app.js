16e3cdfe4fb20acf6b23af6e977636ea63f9f72a
julieoyen <juloye51070@stud.noroff.no>
2024-12-05 16:55:41 +0100

    bug: favicon update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import './css/style.css';
 import router from './js/router/router.js';
 import { setLogoutListener } from './js/ui/global/logout';
-import { addFavicon } from './js/utilities/linkFavicon.js';
 
 await router(window.location.pathname);
 
@@ -9,5 +8,3 @@ const logoutButton = document.getElementsByClassName('logout-btn');
 if (logoutButton) {
   setLogoutListener();
 }
-
-addFavicon();
--- src/js/utilities/linkFavIcon.js
+++
@@ -1,9 +0,0 @@
-import favicon from '/images/favicon.png';
-
-export function addFavicon() {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/png';
-  link.href = favicon;
-  document.head.appendChild(link);
-}
